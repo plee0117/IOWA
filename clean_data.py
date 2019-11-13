c2266c628a1561faf1e4995f8320c1e7c3363220
Paul <emailme.paul@gmail.com>
2019-11-12 21:30:42 -0500

    split feature change function

--- clean_data.py
+++ clean_data.py
@@ -9,35 +9,35 @@ def deal_with_NAs(full_set):
 	Impute missing nominal values with mode
 	Impute missing ordinal and numerical values with median
 	'''
-	categorical_columns = ['MSSubClass','MSZoning','MasVnrType','PoolQC','MiscFeature','Street','Alley','LotShape',\
-						   'LandContour','Utilities','LotConfig','LandSlope','Neighborhood','Condition1','Condition2',\
-						   'BldgType','HouseStyle','RoofStyle','RoofMatl','Exterior1st','Exterior2nd','MasVnrType',\
-						   'ExterQual','ExterCond','Foundation','BsmtQual','BsmtCond','BsmtExposure','BsmtFinType1',\
-						   'BsmtFinType2','Heating','HeatingQC','CentralAir','Electrical','KitchenQual','Functional',\
-						   'FireplaceQu','GarageType','GarageFinish','GarageQual','GarageCond','PavedDrive','Fence',\
+	categorical_columns = ['MSSubClass','MSZoning','MasVnrType','PoolQC','MiscFeature','Street','Alley','LotShape',
+						   'LandContour','Utilities','LotConfig','LandSlope','Neighborhood','Condition1','Condition2',
+						   'BldgType','HouseStyle','RoofStyle','RoofMatl','Exterior1st','Exterior2nd','MasVnrType',
+						   'ExterQual','ExterCond','Foundation','BsmtQual','BsmtCond','BsmtExposure','BsmtFinType1',
+						   'BsmtFinType2','Heating','HeatingQC','CentralAir','Electrical','KitchenQual','Functional',
+						   'FireplaceQu','GarageType','GarageFinish','GarageQual','GarageCond','PavedDrive','Fence',
 						   'SaleType','SaleCondition','MoSold','YrSold']
 
-	numerical_columns = ['LotArea','OverallQual','MasVnrArea','PoolArea','OverallCond','YearBuilt','YearRemodAdd',\
-						 'MasVnrArea','BsmtFinSF1','BsmtFinSF2','BsmtUnfSF','TotalBsmtSF','1stFlrSF','2ndFlrSF',\
-						 'LowQualFinSF','GrLivArea','BsmtFullBath','BsmtHalfBath','FullBath','HalfBath','BedroomAbvGr',\
-						 'KitchenAbvGr','TotRmsAbvGrd','Fireplaces','GarageYrBlt','GarageCars','GarageArea','WoodDeckSF',\
+	numerical_columns = ['LotArea','OverallQual','MasVnrArea','PoolArea','OverallCond','YearBuilt','YearRemodAdd',
+						 'MasVnrArea','BsmtFinSF1','BsmtFinSF2','BsmtUnfSF','TotalBsmtSF','1stFlrSF','2ndFlrSF',
+						 'LowQualFinSF','GrLivArea','BsmtFullBath','BsmtHalfBath','FullBath','HalfBath','BedroomAbvGr',
+						 'KitchenAbvGr','TotRmsAbvGrd','Fireplaces','GarageYrBlt','GarageCars','GarageArea','WoodDeckSF',
 						 'OpenPorchSF','EnclosedPorch','3SsnPorch','ScreenPorch','PoolArea','MiscVal']
 
-	ordinal_columns = ['ExterQual','ExterCond','BsmtQual','BsmtCond','HeatingQC','KitchenQual','FireplaceQu','GarageQual',\
-					   'GarageCond','PoolQC','BsmtExposure','LandSlope','Utilities','LotShape','Functional','Electrical',\
+	ordinal_columns = ['ExterQual','ExterCond','BsmtQual','BsmtCond','HeatingQC','KitchenQual','FireplaceQu','GarageQual',
+					   'GarageCond','PoolQC','BsmtExposure','LandSlope','Utilities','LotShape','Functional','Electrical',
 					  'GarageFinish','PavedDrive','Fence','BsmtFinType1','BsmtFinType2']
 
 	nominal_columns = list(set(categorical_columns)-set(ordinal_columns))
 
-	ordinal_nas = ['ExterQual','ExterCond','BsmtQual','BsmtCond','HeatingQC','KitchenQual','FireplaceQu','GarageQual',\
+	ordinal_nas = ['ExterQual','ExterCond','BsmtQual','BsmtCond','HeatingQC','KitchenQual','FireplaceQu','GarageQual',
 				   'GarageCond','PoolQC','BsmtExposure','GarageFinish','Fence','BsmtFinType1','BsmtFinType2']
 
 	nominal_nas = ['Alley', 'GarageType', 'MiscFeature']
 
-	q_to_q = {'Ex':5,'Gd':4,'TA':3,'Fa':2,'Po':1,'Av':3,'Mn':2,'No':1,'Gtl':2,'Mod':1,'Sev':0,\
-			  'AllPub':3,'NoSewr':2,'NoSeWa':1,'ELO':0,'Reg':3,'IR1':2,'IR2':1,'IR3':0,\
-			  'Typ':7,'Min1':6,'Min2':5,'Mod':4,'Maj1':3,'Maj2':2,'Sev':1,'Sal':0,'SBrkr':4,\
-			  'FuseA':3,'FuseF':2,'FuseP':1,'Mix':0,'Fin':3,'RFn':2,'Unf':1,'Y':2,'P':1,'N':0,\
+	q_to_q = {'Ex':5,'Gd':4,'TA':3,'Fa':2,'Po':1,'Av':3,'Mn':2,'No':1,'Gtl':2,'Mod':1,'Sev':0,
+			  'AllPub':3,'NoSewr':2,'NoSeWa':1,'ELO':0,'Reg':3,'IR1':2,'IR2':1,'IR3':0,
+			  'Typ':7,'Min1':6,'Min2':5,'Mod':4,'Maj1':3,'Maj2':2,'Sev':1,'Sal':0,'SBrkr':4,
+			  'FuseA':3,'FuseF':2,'FuseP':1,'Mix':0,'Fin':3,'RFn':2,'Unf':1,'Y':2,'P':1,'N':0,
 			  'GdPrv':4,'MnPrv':3,'GdWo':2,'MnWw':1,'GLQ':6,'ALQ':5,'BLQ':4,'Rec':3,'LwQ':2}
 
 	# Convert ordinal feature values from qualitative to quantitative except Na's
@@ -78,22 +78,21 @@ def deal_with_NAs(full_set):
 
 
 
-
 def ordinal_features(full_set):
 	'''
 	Convert categorical into ordinal and deal with its Na's
 	'''
-	ordinal_columns = ['ExterQual','ExterCond','BsmtQual','BsmtCond','HeatingQC','KitchenQual','FireplaceQu','GarageQual',\
-					   'GarageCond','PoolQC','BsmtExposure','LandSlope','Utilities','LotShape','Functional','Electrical',\
+	ordinal_columns = ['ExterQual','ExterCond','BsmtQual','BsmtCond','HeatingQC','KitchenQual','FireplaceQu','GarageQual',
+					   'GarageCond','PoolQC','BsmtExposure','LandSlope','Utilities','LotShape','Functional','Electrical',
 					  'GarageFinish','PavedDrive','Fence','BsmtFinType1','BsmtFinType2']
 
-	ordinal_nas = ['ExterQual','ExterCond','BsmtQual','BsmtCond','HeatingQC','KitchenQual','FireplaceQu','GarageQual',\
+	ordinal_nas = ['ExterQual','ExterCond','BsmtQual','BsmtCond','HeatingQC','KitchenQual','FireplaceQu','GarageQual',
 				   'GarageCond','PoolQC','BsmtExposure','GarageFinish','Fence','BsmtFinType1','BsmtFinType2']
 
-	q_to_q = {'Ex':5,'Gd':4,'TA':3,'Fa':2,'Po':1,'Av':3,'Mn':2,'No':1,'Gtl':2,'Mod':1,'Sev':0,\
-			  'AllPub':3,'NoSewr':2,'NoSeWa':1,'ELO':0,'Reg':3,'IR1':2,'IR2':1,'IR3':0,\
-			  'Typ':7,'Min1':6,'Min2':5,'Mod':4,'Maj1':3,'Maj2':2,'Sev':1,'Sal':0,'SBrkr':4,\
-			  'FuseA':3,'FuseF':2,'FuseP':1,'Mix':0,'Fin':3,'RFn':2,'Unf':1,'Y':2,'P':1,'N':0,\
+	q_to_q = {'Ex':5,'Gd':4,'TA':3,'Fa':2,'Po':1,'Av':3,'Mn':2,'No':1,'Gtl':2,'Mod':1,'Sev':0,
+			  'AllPub':3,'NoSewr':2,'NoSeWa':1,'ELO':0,'Reg':3,'IR1':2,'IR2':1,'IR3':0,
+			  'Typ':7,'Min1':6,'Min2':5,'Mod':4,'Maj1':3,'Maj2':2,'Sev':1,'Sal':0,'SBrkr':4,
+			  'FuseA':3,'FuseF':2,'FuseP':1,'Mix':0,'Fin':3,'RFn':2,'Unf':1,'Y':2,'P':1,'N':0,
 			  'GdPrv':4,'MnPrv':3,'GdWo':2,'MnWw':1,'GLQ':6,'ALQ':5,'BLQ':4,'Rec':3,'LwQ':2}
 
 	# Convert ordinal feature values from qualitative to quantitative except Na's
@@ -138,14 +137,14 @@ def impute_num_median(full_set):
 	Deal with actual missing values aka Na's
 	'''
 
-	numerical_columns = ['LotArea','OverallQual','MasVnrArea','PoolArea','OverallCond','YearBuilt','YearRemodAdd',\
-						 'MasVnrArea','BsmtFinSF1','BsmtFinSF2','BsmtUnfSF','TotalBsmtSF','1stFlrSF','2ndFlrSF',\
-						 'LowQualFinSF','GrLivArea','BsmtFullBath','BsmtHalfBath','FullBath','HalfBath','BedroomAbvGr',\
-						 'KitchenAbvGr','TotRmsAbvGrd','Fireplaces','GarageYrBlt','GarageCars','GarageArea','WoodDeckSF',\
+	numerical_columns = ['LotArea','OverallQual','MasVnrArea','PoolArea','OverallCond','YearBuilt','YearRemodAdd',
+						 'MasVnrArea','BsmtFinSF1','BsmtFinSF2','BsmtUnfSF','TotalBsmtSF','1stFlrSF','2ndFlrSF',
+						 'LowQualFinSF','GrLivArea','BsmtFullBath','BsmtHalfBath','FullBath','HalfBath','BedroomAbvGr',
+						 'KitchenAbvGr','TotRmsAbvGrd','Fireplaces','GarageYrBlt','GarageCars','GarageArea','WoodDeckSF',
 						 'OpenPorchSF','EnclosedPorch','3SsnPorch','ScreenPorch','PoolArea','MiscVal']
 
-	ordinal_columns = ['ExterQual','ExterCond','BsmtQual','BsmtCond','HeatingQC','KitchenQual','FireplaceQu','GarageQual',\
-					   'GarageCond','PoolQC','BsmtExposure','LandSlope','Utilities','LotShape','Functional','Electrical',\
+	ordinal_columns = ['ExterQual','ExterCond','BsmtQual','BsmtCond','HeatingQC','KitchenQual','FireplaceQu','GarageQual',
+					   'GarageCond','PoolQC','BsmtExposure','LandSlope','Utilities','LotShape','Functional','Electrical',
 					  'GarageFinish','PavedDrive','Fence','BsmtFinType1','BsmtFinType2']
 
 	nas = np.sum(full_set.isna()).reset_index()
@@ -172,16 +171,16 @@ def impute_cat_mode(full_set):
 	Deal with actual missing values aka Na's
 	'''
 
-	categorical_columns = ['MSSubClass','MSZoning','MasVnrType','PoolQC','MiscFeature','Street','Alley','LotShape',\
-						   'LandContour','Utilities','LotConfig','LandSlope','Neighborhood','Condition1','Condition2',\
-						   'BldgType','HouseStyle','RoofStyle','RoofMatl','Exterior1st','Exterior2nd','MasVnrType',\
-						   'ExterQual','ExterCond','Foundation','BsmtQual','BsmtCond','BsmtExposure','BsmtFinType1',\
-						   'BsmtFinType2','Heating','HeatingQC','CentralAir','Electrical','KitchenQual','Functional',\
-						   'FireplaceQu','GarageType','GarageFinish','GarageQual','GarageCond','PavedDrive','Fence',\
+	categorical_columns = ['MSSubClass','MSZoning','MasVnrType','PoolQC','MiscFeature','Street','Alley','LotShape',
+						   'LandContour','Utilities','LotConfig','LandSlope','Neighborhood','Condition1','Condition2',
+						   'BldgType','HouseStyle','RoofStyle','RoofMatl','Exterior1st','Exterior2nd','MasVnrType',
+						   'ExterQual','ExterCond','Foundation','BsmtQual','BsmtCond','BsmtExposure','BsmtFinType1',
+						   'BsmtFinType2','Heating','HeatingQC','CentralAir','Electrical','KitchenQual','Functional',
+						   'FireplaceQu','GarageType','GarageFinish','GarageQual','GarageCond','PavedDrive','Fence',
 						   'SaleType','SaleCondition','MoSold','YrSold']
 
-	ordinal_columns = ['ExterQual','ExterCond','BsmtQual','BsmtCond','HeatingQC','KitchenQual','FireplaceQu','GarageQual',\
-					   'GarageCond','PoolQC','BsmtExposure','LandSlope','Utilities','LotShape','Functional','Electrical',\
+	ordinal_columns = ['ExterQual','ExterCond','BsmtQual','BsmtCond','HeatingQC','KitchenQual','FireplaceQu','GarageQual',
+					   'GarageCond','PoolQC','BsmtExposure','LandSlope','Utilities','LotShape','Functional','Electrical',
 					  'GarageFinish','PavedDrive','Fence','BsmtFinType1','BsmtFinType2']
 
 	nominal_columns = list(set(categorical_columns)-set(ordinal_columns))
@@ -207,6 +206,13 @@ def impute_cat_mode(full_set):
 
 def change_features(full_set):
 	'''
+	Create:
+	PropConsLot
+	LivingArea
+	ConstArea
+	Yard
+	YardPerc
+
 	Combine:
 	3SsnPorch, EnclosedPorch, ScreenPorch, OpenPorchSF, and WoodDeckSF to PorchType
 	PoolArea and PoolQC to PoolYN
@@ -224,14 +230,42 @@ def change_features(full_set):
 	MSSubClass
 	'''
 
+	# Add new feature
+	if all([x in full_set.columns for x in ['1stFlrSF','LotArea']]):
+		full_set['PropConsLot'] = full_set['1stFlrSF'] / full_set['LotArea']
+
+	if all([x in full_set.columns for x in ['GarageType']]):
+		full_set['Garage'] = 0
+		full_set['Garage'] = np.where((full_set['GarageType'] == 'BuiltIn'),1,full_set['Garage'])
+		full_set['Garage'] = np.where((full_set['GarageType'] == 'Basment'),1,full_set['Garage'])
+		full_set['Garage'] = np.where((full_set['GarageType'] == '2Types'),0.5,full_set['Garage'])
+
+	if all([x in full_set.columns for x in ['TotalBsmtSF','GrLivArea','GarageArea','Garage']]):
+		full_set['LivingArea'] = full_set['TotalBsmtSF'] + full_set['GrLivArea'] + full_set['GarageArea'] * full_set['Garage']
+
+	if all([x in full_set.columns for x in ['1stFlrSF','GarageArea','Garage']]):
+		full_set['ConstArea'] = full_set['1stFlrSF'] + full_set['GarageArea'] * full_set['Garage']
+
+	if all([x in full_set.columns for x in ['GrLivArea','LotArea']]):  #!!!!!! should we keep this due to multicollinearity????
+		full_set['ConstCost'] = full_set['GrLivArea'] * 100 + 4 * full_set['LotArea']
+
+	if all([x in full_set.columns for x in ['LotArea','ConstArea']]):  #!!!!!! should we keep this due to multicollinearity????
+		full_set['Yard'] = 0
+		full_set['Yard'] = full_set['LotArea'] - full_set['ConstArea']
+
+	if all([x in full_set.columns for x in ['ConstArea','LotArea']]):
+		full_set['YardPerc'] = full_set['ConstArea'] / full_set['LotArea']
+
 	# Combine 3SsnPorch, EnclosedPorch, ScreenPorch, OpenPorchSF, and WoodDeckSF into 1 ordinal feature ranking the types
 	if all( x in full_set.columns for x in ['3SsnPorch','EnclosedPorch','ScreenPorch','OpenPorchSF','WoodDeckSF']):
-		full_set['PorchType'] = pd.DataFrame(5 if full_set['3SsnPorch'][i] > 0 else \
-										 4 if full_set['EnclosedPorch'][i] > 0 else \
-										 3 if full_set['ScreenPorch'][i] > 0 else \
-										 2 if full_set['OpenPorchSF'][i] > 0 else \
-										 1 if full_set['WoodDeckSF'][i]> 0 else 0 \
+		full_set['PorchType'] = pd.DataFrame(5 if full_set['3SsnPorch'][i] > 0 else 
+										 4 if full_set['EnclosedPorch'][i] > 0 else 
+										 3 if full_set['ScreenPorch'][i] > 0 else 
+										 2 if full_set['OpenPorchSF'][i] > 0 else 
+										 1 if full_set['WoodDeckSF'][i]> 0 else 0 
 										 for i in range(1,len(full_set.WoodDeckSF)))
+		full_set['PorchArea'] = (full_set['3SsnPorch'] + full_set['EnclosedPorch'] + full_set['ScreenPorch'] + 
+			full_set['OpenPorchSF'] + full_set['WoodDeckSF'])
 		full_set.drop(columns = ['3SsnPorch', 'EnclosedPorch', 'ScreenPorch', 'OpenPorchSF', 'WoodDeckSF'], inplace = True)
 
 	# Combine PoolArea and PoolQC to PoolYN
@@ -275,23 +309,35 @@ def change_features(full_set):
 	if 'MSSubClass' in full_set.columns:
 		full_set.drop(columns = 'MSSubClass', inplace = True)
 
-	# Add new feature
-	if all( x in full_set.columns for x in ['1stFlrSF','LotArea']):
-		full_set['PropConsLot'] = full_set['1stFlrSF'] / full_set['LotArea']
+	return full_set
 
-	# Label encoding nominal features
-	categorical_columns = ['MSSubClass','MSZoning','MasVnrType','PoolQC','MiscFeature','Street','Alley','LotShape',\
-						   'LandContour','Utilities','LotConfig','LandSlope','Neighborhood','Condition1','Condition2',\
-						   'BldgType','HouseStyle','RoofStyle','RoofMatl','Exterior1st','Exterior2nd','MasVnrType',\
-						   'ExterQual','ExterCond','Foundation','BsmtQual','BsmtCond','BsmtExposure','BsmtFinType1',\
-						   'BsmtFinType2','Heating','HeatingQC','CentralAir','Electrical','KitchenQual','Functional',\
-						   'FireplaceQu','GarageType','GarageFinish','GarageQual','GarageCond','PavedDrive','Fence',\
-						   'SaleType','SaleCondition','MoSold','YrSold']
-	ordinal_columns = ['ExterQual','ExterCond','BsmtQual','BsmtCond','HeatingQC','KitchenQual','FireplaceQu','GarageQual',\
-					   'GarageCond','PoolQC','BsmtExposure','LandSlope','Utilities','LotShape','Functional','Electrical',\
+def label_encode(full_set):
+	'''
+	Label encoding nominal features
+	'''
+	categorical_columns = []
+
+	for col in full_set.columns:
+		if full_set[col].dtype in ['object']:
+			categorical_columns.append(col)
+
+	ordinal_columns = ['ExterQual','ExterCond','BsmtQual','BsmtCond','HeatingQC','KitchenQual','FireplaceQu','GarageQual',
+					   'GarageCond','PoolQC','BsmtExposure','LandSlope','Utilities','LotShape','Functional','Electrical',
 					  'GarageFinish','PavedDrive','Fence','BsmtFinType1','BsmtFinType2']
+
 	nominal_columns = list(set(categorical_columns)-set(ordinal_columns))
 	nominal_kept = set(nominal_columns) & set(full_set.columns)
 	full_set = pd.get_dummies(full_set, prefix_sep = '_', columns=nominal_kept, drop_first = True)
 
+	return full_set	
+
+
+def boxcox_these(full_set):
+	boxcoxing = ['ConstArea', 'LivingArea', 'YardPerc', 'LotArea', '2ndFlrSF', 'BsmtUnfSF', 'BsmtFinSF1', 'LowQualFinSF', '1stFlrSF', 
+	'MiscVal', 'BsmtFinSF2', 'Yard', 'ConstCost', 'PropConsLot']
+	boxcoxing = list(set(boxcoxing) & set(full_set.columns))
+	for i in boxcoxing:
+		full_set[[i]] = boxcox(full_set[[i]] + 1)[0]
+
 	return full_set
+
