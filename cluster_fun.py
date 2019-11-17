c3d3cee39c5678f4311abf1290092ee2c59155e7
Fatema <fatemafsoliman@gmail.com>
2019-11-17 13:40:19 -0500

    updating cluster function, adding data processing file with lot imputation and residual plots

--- cluster_fun.py
+++ cluster_fun.py
@@ -18,20 +18,22 @@ def cluster(n, df):
 
     #MAKE LABELS OF CATEGORICAL VARIABLE BECAUSE K MEANS CAN'T HANDLE STRINGS
 
-    
-    lbl = df[xvar].unique()
-    lbl = pd.DataFrame(lbl)
-    lbl['mylablels']= list(range(1,len(lbl)+1))
-    lbl.columns = [xvar, 'mylabels']
-    df = df.merge(lbl, on = xvar)
-    df = df[["mylabels", yvar]]
-    df_means = df.groupby("mylabels").agg("mean").reset_index().sort_values(yvar)
-    df_array = df_means.to_numpy()
-    
-    
+
+    df_means_og = df.groupby(xvar).agg("mean").reset_index().sort_values(yvar).reset_index()
+    df_x = df_means_og[xvar]
+    df_y = df_means_og[yvar]
+
+
+    ary = np.array(df_y)
+
     #APPLY K MEANS
-    kfit = KMeans(n_clusters=n, random_state=0).fit(df_array)
+    kfit = KMeans(n_clusters=n, random_state=0).fit(ary.reshape(-1,1))
     klabels = kfit.labels_
+    klab_df = pd.DataFrame(klabels)
+
+    ary_df = pd.DataFrame(df_y)
+    ary_df['klab'] = klabels
+    ary_df[xvar] = df_x
 
     u, ind = np.unique(klabels,return_index=True)
     u = u[np.argsort(ind)]
@@ -39,18 +41,15 @@ def cluster(n, df):
     kordered_dict = dict(zip(u, klabels_ordered))
     
     
-    k_df = pd.DataFrame(df_array)
+    k_df = pd.DataFrame(df_x)
     k_df['klabels'] = klabels
     k_df['klabels'] = pd.Series(k_df.klabels.map(kordered_dict))
     colnames = df.columns
-    k_df.columns = [colnames[0], yvar, 'klabels']
     
-   
-    #TRANSFORM TO A DATAFRAME LOOKUP OF LABELS TO ORIGINAL VARIABLE 
-    kDict = k_df[['klabels', 'mylabels', yvar]]
-    kDict = k_df.set_index('klabels').sort_values(yvar).reset_index()
-    kDict = k_df.merge(lbl, on= 'mylabels')[['klabels',xvar, yvar]].sort_values(yvar) #Sale price is mean in each category 
-    
+    k_df.columns = [xvar, 'klabels']
+
+    kDict = k_df
+
     return kDict
 
 
@@ -80,7 +79,7 @@ def kReplace(df, kdf, xvar, yvar):
     ktargetname = 'k' + xvar
     kData[ktargetname]= ""
     
-    kdf = kdf.drop(yvar, axis = 1)
+    #kdf = kdf.drop(yvar, axis = 1)
     kdf = kdf.set_index(xvar).transpose().to_dict(orient='list')
    
     for k, v in kdf.items():
@@ -89,16 +88,5 @@ def kReplace(df, kdf, xvar, yvar):
     for i in np.arange(len(df)):
         kData[ktargetname].loc[i]  = kdf.get(df[xvar].loc[i])
         
-    kData = kData.drop(xvar, axis = 1)
-    kData = kData.rename(columns={ktargetname: xvar})
-    
-    #for i in np.arange(len(kData)):
-    #    kData[xvar].loc[i] = kData[xvar].loc[i][0]
-    
     return kData
-
-
-#Example:
-#print(kReplace(data, test, 'Neighborhood', 'SalePrice'))
-
 
